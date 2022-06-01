cee39cbed33868cf482ccd520774000affea1c43
bille.suresh <bille.suresh@globallogic.com>
2022-06-01 15:36:16 +0530

    user service

--- authentication-service/src/main/java/com/stackroute/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/stackroute/AuthenticationServiceApplication.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  import org.springframework.context.annotation.Bean;
  import springfox.documentation.builders.ApiInfoBuilder;
  import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,28 +14,15 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  import springfox.documentation.service.Contact;
  import springfox.documentation.spi.DocumentationType;
  import springfox.documentation.spring.web.plugins.Docket;
+ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 
-//import org.springframework.context.annotation.Bean;
- 
-//import io.swagger.annotations.AuthorizationScope;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.ApiKey;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.service.SecurityReference;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spi.service.contexts.SecurityContext;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
- 
-//@SpringBootApplication
-//@EnableSwagger2
-//>>>>>>> 8ee1526337e0eec12cdbd7a4a65a065355c02116
+
+
+@EnableSwagger2
+
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
