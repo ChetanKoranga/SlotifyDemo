ddcebd8ba8a61bbccebb5e29611acfcf31c48f85
unknown <mandapati.reddy@niit.com>
2022-06-10 09:55:22 +0530

    resolved api-gateway

--- email-service/src/main/java/com/stackroute/consumer/ConsumerListener.java
+++ email-service/src/main/java/com/stackroute/consumer/ConsumerListener.java
@@ -8,7 +8,6 @@ package com.stackroute.consumer;
 import com.stackroute.config.MessagingConfig;
 import com.stackroute.model.Email;
 import com.stackroute.service.EmailService;
-import org.modelmapper.ModelMapper;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -19,9 +18,20 @@ public class ConsumerListener {
     @RabbitListener(queues = MessagingConfig.EMAIL_NOTIFICATION_QUEUE)
     public void emailToInterviewer(ConsumerDto consumerDTO) throws Exception {
         try {
-            ModelMapper modelMapper = new ModelMapper();
+
             EmailService emailService = new EmailService();
-              Email email = modelMapper.map(consumerDTO,Email.class);
+            Email email = new Email();
+            email.setCandidateEmailId(consumerDTO.getCandidateEmailId());
+            email.setIntervierEmailId(consumerDTO.getIntervierEmailId());
+            email.setCandidatename(consumerDTO.getCandidatename());
+            email.setInterviername(consumerDTO.getInterviername());
+            email.setTagEmailId(consumerDTO.getTagEmailId());
+            email.setDate(consumerDTO.getDate());
+            email.setStartTime(consumerDTO.getStartTime());
+            email.setMessageText(consumerDTO.getMessageText());
+            email.setEndTime(consumerDTO.getEndTime());
+            email.setSubject(consumerDTO.getSubject());
+            email.setTagmembername(consumerDTO.getTagmembername());
             emailService.sendEmail(email);
 
         } catch (Exception e) {
--- user-service/src/main/java/com/stackroute/controllers/userController.java
+++ user-service/src/main/java/com/stackroute/controllers/userController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/userservice")
+@RequestMapping("/api/v1")
 public class userController {
 
     @Autowired
